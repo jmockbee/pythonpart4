5b1bfd3acfbea182bfdc4cda52883b2a24af568b
Jon Mockbee <mockbee1000@gmail.com>
2021-07-17 16:39:47 -0400

    new changes

--- labpythonoperat.py
+++ labpythonoperat.py
@@ -2,5 +2,7 @@ var_a = float(input("input a number: "))
 var_b  = float(input("input a number: "))
 
 print ("result of addition here " + str(var_a + var_b))
-
+print ("result of subtraction here " + str(var_a - var_b))
+print ("result of multiplication here " + str(var_a * var_b))
+print ("result of division here " + str(var_a / var_b))
 
